8fb0f0ae98522ce227b659b3112dfa243e8ccffa
Moazz <moazzemh222@gmail.com>
2022-12-21 21:03:35 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    class Income
+    {
+        public int _income = 45000;
+        public int income { 
+            get { return _income; } 
+            set { 
+                if(value <= 0)
+                {
+                    _income = 0;
+                }
+                else if(value > 45000 )
+                {
+                    _income = 45000;
+                }
+                else
+                {
+                    _income = value;
+                }
+            } 
+        }
+        public void Debit(int moneySpent)
+        {
+            _income = _income - moneySpent;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Income amountLeft = new Income();
+            amountLeft.Debit(25000);
+            Console.WriteLine(amountLeft.income);
+            amountLeft.income = amountLeft.income - 50000;
+            Console.WriteLine(amountLeft.income);
+            amountLeft.income += 70000;
+            Console.WriteLine(amountLeft.income);
+            Console.ReadKey();
+        }
+    }
+}
